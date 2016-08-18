1094b8e1b085b95dc3cdd7fc752a031a3375b761
yindolia <yindolia@gmail.com>
2016-08-18 17:49:39 +0530

    Create SampleProducer.scala

---
+++ SampleProducer.scala
@@ -0,0 +1,58 @@
+package Kafka.producer
+
+import java.util.{Properties, UUID}
+import kafka.common._
+import kafka.message._
+import kafka.producer.Producer
+
+case class KafkaProducer(
+
+    topic: String,
+    brokerList: String,
+    clientId: String= UUID.randomUUID().toString,
+    synchronously: Boolean =true,
+    batchSize: Int = 200,
+    messageSendMaxRetries: Int = 3,
+    requestRequireAcks: Int = -1
+   ) {
+  
+  val props = new Properties()
+ 
+  
+  
+  props.put("producer.type", if (synchronously) "sync" else "async")
+  props.put("metadata.broker.list", brokerList)
+  props.put("batch.num.messages", batchSize.toString)
+  props.put("message.send.max.retries", messageSendMaxRetries.toString)
+  props.put("request.require.acks", requestRequireAcks.toString)
+  props.put("client.id", clientId.toString)
+
+
+  val config = new ProducerConfig (props)
+  val producer = new Producer[AnyRef, AnyRef](config)
+
+
+
+  def kafkaMessage(message: Array[Byte], partition: Array[Byte]): KeyedMessage[AnyRef, AnyRef] = {
+// KeyedMessage:  Part of kafka Core
+    if (partition== null) {
+      new KeyedMessage(topic, message)
+    }
+    else {
+      new KeyedMessage(topic, partition, message)
+    }
+
+
+  }
+  def sendMessage(message: String, partition: String = null): Unit = {
+    try {
+      producer.send(kafkaMessage(message, partition))
+    }
+    catch {
+      case e : Exception =>
+        e.printStackTrace
+        System.exit(1)
+    }
+  }
+
+}
