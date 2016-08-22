62a32686b770fde227bc224264c84b09a7cd7752
yindolia <yindolia@gmail.com>
2016-08-22 17:14:52 +0530

    Update Partitioner.scala

--- Partitioner.scala
+++ Partitioner.scala
@@ -2,3 +2,21 @@ package Kafka.producer
 
 import kafka.producer.Partitioner
 import kafka.utils.VerifiableProperties
+
+class simplePartioner extends Partitioner{
+  def simplepartitioner (props: VerifiableProperties): Null= Null
+  
+  def partition(key: Any, npart: Int): Int = {
+    val partition = 0
+    val stringKey = key.toString;
+    val offset = stringKey.lastIndexOf('.')
+    if (offset >0) {
+      partition = Integer.parseInt(stringKey.substring(offset+1))% npart
+    }
+    else partition = 0
+    
+    return partition
+    
+  }
+  
+}
